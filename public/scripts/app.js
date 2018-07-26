1248387b10699734de5f97e12e177085c980eff7
Rafael <rafaelmarianorocha@gmail.com>
2018-07-26 17:51:12 +0000

    Adds order query with details of order.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,13 @@
 $(() => {
 
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
 
   $.ajax({
     method: "GET",
@@ -16,8 +16,14 @@ $(() => {
     console.log(orders[1]);
     for(order of orders) {
       console.log(order);
+      if(order.imgurl){
       $("<div>").text(order.name).appendTo($("body"))
+      $('body').append(`<img src="${order.imgurl}" height="120px" width="120px">`);
       $("<div>").text(order.count).appendTo($("body"));
+    } else{
+      $("<div>").text(`Hi ${order.name}! Here's your order:`).appendTo($("body"))
+      $("h1").append(`${order.id} Status:`)
+    }
     }
   });;
 
--- routes/orders.js
+++ routes/orders.js
@@ -6,17 +6,17 @@ const router  = express.Router();
 module.exports = (knex) => {
 //send results obj with foods ordered and quantity;
   router.get("/", (req, res) => {
-    knex("orders")
+          knex("orders")
       .leftJoin("order_foods", "orders.id", "order_foods.order_id").leftJoin("foods", "order_foods.food_id", "foods.id")
-      .select("foods.name").count('order_foods.order_id')
+      .distinct("orders.name","orders.id")
+      .select()
       .where({order_id: 1})
-      .groupBy('foods.name')
       .then((result)=>{
         knex("orders")
       .leftJoin("order_foods", "orders.id", "order_foods.order_id").leftJoin("foods", "order_foods.food_id", "foods.id")
-      .distinct("orders.name")
-      .select()
+      .select("foods.imgurl","foods.name").count('order_foods.order_id')
       .where({order_id: 1})
+      .groupBy('foods.name','foods.imgurl')
       .then((newResult)=>{
         let newArray = result.concat(newResult);
         res.json(newArray);
