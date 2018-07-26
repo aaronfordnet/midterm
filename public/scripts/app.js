4b430144433c29eb12b37a5aeb31b83b11bc244c
Aaron Ford <aaron@aaronford.net>
2018-07-26 17:33:54 +0000

    added layout to index page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,11 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/api/apiroutes"
+  }).done((results) => {
+    for(result of results) {
+      console.log(result);
+      $("<div>").text(result.name).appendTo($("body"));
     }
-  });;
+  });
 });
