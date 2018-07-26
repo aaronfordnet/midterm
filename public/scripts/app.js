b7b28c61687073af97639ce097d8b4d9a60a9fcc
Rafael <rafaelmarianorocha@gmail.com>
2018-07-26 04:21:33 +0000

    Adds queries to get order list and customer name

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,5 @@
 $(() => {
+
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -7,4 +8,25 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  $.ajax({
+    method: "GET",
+    url: "/api/orders"
+  }).done((orders) => {
+    console.log(orders[1]);
+    for(order of orders) {
+      console.log(order);
+      $("<div>").text(order.name).appendTo($("body"))
+      $("<div>").text(order.count).appendTo($("body"));
+    }
+  });;
+
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/foods"
+  // }).done((foods) => {
+  //   for(food of foods) {
+  //     $("<div>").text(food.name).appendTo($("body"));
+  //   }
+  // });;
 });
