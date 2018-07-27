33844e0524443269666e52e103ab5d90b8767f03
Rafael <rafaelmarianorocha@gmail.com>
2018-07-27 23:50:06 +0000

    Adds total to order page

--- public/scripts/app_orderstatus.js
+++ public/scripts/app_orderstatus.js
@@ -3,16 +3,16 @@
 
 $(() => {
 
-    // for (let order of data) {
-    //   console.log(order);
-    //   if (order.imgurl) {
-    //     $("<div>").text(order.name).appendTo($("body"));
-    //     $('body').append(`<img src="${order.imgurl}" height="120px" width="120px">`);
-    //     $("<div>").text(order.count).appendTo($("body"));
-    //   } else {
-    //     $("<div>").text(`Hi ${order.name}! Here's your order:`).appendTo($("body"));
-    //     $("h1").append(`${order.id} Status:`);
-    //   }
-    // }
+  function getTotal() {
+    var total = 0;
+    $('.sum').each(function() {
+      let amount = Number(this.innerHTML.replace("$",""));
+      console.log(amount)
+      total += amount
+    });
+    $('#order-total').text(total.toFixed(2));
+  }
+
+  getTotal();
 
 });
