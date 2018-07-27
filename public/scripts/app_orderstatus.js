a4fcd4972dfebb843e5d6f0b6f55ab0b2c148137
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-07-27 08:32:29 -0700

    replaces removed files

---
+++ public/scripts/app_orderstatus.js
@@ -0,0 +1,18 @@
+// jshint esversion: 6
+// "use strict";
+
+$(() => {
+
+    // for (let order of data) {
+    //   console.log(order);
+    //   if (order.imgurl) {
+    //     $("<div>").text(order.name).appendTo($("body"));
+    //     $('body').append(`<img src="${order.imgurl}" height="120px" width="120px">`);
+    //     $("<div>").text(order.count).appendTo($("body"));
+    //   } else {
+    //     $("<div>").text(`Hi ${order.name}! Here's your order:`).appendTo($("body"));
+    //     $("h1").append(`${order.id} Status:`);
+    //   }
+    // }
+
+});
