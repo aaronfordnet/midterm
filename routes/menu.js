9fa3e90314ea71f2348f98b85e8b50e750a28fde
Rafael <rafaelmarianorocha@gmail.com>
2018-07-29 19:48:10 +0000

    Adds styling to border radius. Fixed favourites section. Foods and drinks wont repeat themselves.

--- routes/menu.js
+++ routes/menu.js
@@ -16,5 +16,20 @@ module.exports = (knex) => {
     });
   });
 
+  router.get("/favourites", (req, res) => {
+    knex
+      .select("foods.id", "foods.name", "foods.description", "foods.imgurl", "foods.price", "foods.category").count("order_foods.food_id")
+      .from("foods")
+      .leftJoin("order_foods", "foods.id", "order_foods.food_id")
+      .groupBy("foods.id", "foods.name", "foods.description", "foods.imgurl", "foods.price", "foods.category")
+      .orderBy(knex.raw('count(order_foods.food_id)'), 'desc')
+      .limit(3)
+      // .having(knex.raw(('count(order_foods.food_id) > 2')))
+      .then((results) => {
+        console.log(results)
+        res.json(results);
+    });
+  });
+
   return router;
 };
